e5aa6f97d46a857565cf67e1ff1c1132d9761c14
Maxim <bychikhinma@gmail.com>
2022-01-24 20:38:50 +0700

    second commit

--- index.py
+++ index.py
@@ -18,3 +18,17 @@ for friends in user.get('friends'):
         girls_drivers.append({friends['name']})
        
 
+best_occupation = []
+max_salary = 0
+
+for user in users:
+    friends = user.get('friends', [])
+    for friend in friends:
+        job = friend['job']
+        salary = job['salary']
+        if salary > max_salary:
+            max_salary = job['salary']
+            best_occupation.append({'occupation': job['occupation'], 'salary': job['salary']})
+            print(best_occupation)
+
+pprint(user)
